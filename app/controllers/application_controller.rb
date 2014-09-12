b377239d8d269baa40ba2fa0064999f0ea93171c
Scott Meade <scott@synapsoftware.com>
2014-09-12 09:41:24 -0600

    Add phone variant of products list
    - create set_variant before action
    - create phone variant of index.html
    - update controller action to respond_to variants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_variant
+
+  private
+    def set_variant
+      case request.user_agent
+      when /Phone/i
+        request.variant = :phone
+      when /Android/i && /mobile/i
+        request.variant = :phone
+      end
+    end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,11 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    respond_to do |format|
+      format.json
+      format.html          # index.html.erb
+      format.html.phone    # index.html+phone.erb
+    end
   end
 
   # GET /products/1
